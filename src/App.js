f8aa8045a6bb25cb6e7f2c0ff677fd2e3d78c954
aparnah18 <aparnah18@gmail.com>
2020-12-29 21:16:48 +0530

    Created Skeleton of Home Page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
+import { render } from '@testing-library/react';
+import {Fragment} from 'react';
 import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div>
-      Phone Directory
+class App extends Component {
+  render() {
+    return (
+      <Fragment>
+      <div>
+        <div className="header">
+          Phone Directory
+        </div>
+        <button>Add</button>
+        <div>
+          <span>Name</span><br />
+          <span>Phone</span>
+        </div>
     </div>
-  );
+    </Fragment>
+    );
+  }
 }
 
 export default App;
