670465e875921a991a3b5cc81cffba29321a20cd
aparnah18 <aparnah18@gmail.com>
2021-01-04 20:47:26 +0530

    Changed ShowSubscribers.js

--- src/ShowSubscribers.js
+++ src/ShowSubscribers.js
@@ -2,29 +2,9 @@ import React, { Component } from 'react';
 import Header from './Header.js';
 import './ShowSubscribers.css';
 
-class App extends Component {
-
-  constructor() {
-    super();
-    this.state = {
-      subscribersListToShow: []
-    }
-  }
+class ShowSubscribers extends Component {
 
   render() {
-
-    let subscribers = [
-      {
-        id: 1,
-        name: "Shilpa Bhat",
-        phone: "8888888888"
-      },
-      {
-        id: 2,
-        name: "Srishti Gupta",
-        phone: "9999999999"
-      }
-    ];
 
     return (
       <div>
@@ -38,7 +18,7 @@ class App extends Component {
           </div>
 
           {
-            subscribers.map(sub => {
+            this.props.subscribersList.map(sub => {
               return <div key={sub.id} className="grid-container">
                 <span className="grid-item">{sub.name}</span>
                 <span className="grid-item">{sub.phone}</span>
@@ -54,4 +34,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default ShowSubscribers;
